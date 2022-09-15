b1aa079f941815fbd3f409281e4060873bc912d0
William Dawson <william.dawson@riken.jp>
2022-09-15 20:45:03 +0900

    No magic needed

--- nbup.py
+++ nbup.py
@@ -1,19 +1,19 @@
-from IPython.core.magics.display import Javascript
+from os import system
 _base = "https://github.com/william-dawson/test-colab/raw/main/packages/"
 _pkg = "bigdft-suite-1.9.3_rc.2-mpi_openmpi_py37h008c211_0.tar.bz2"
 
 def download():
-    return Javascript("wget " + _base + "/" + _pkg)
+    system("wget " + _base + "/" + _pkg)
 
 def install_colab():
-    return Javascript("!pip install -q condacolab")
+    system("pip install -q condacolab")
 
 def setup_colab():
     import condacolab
     condacolab.install()
 
 def install_bigdft():
-   return javascript("! conda install " + _pkg + " ; conda update --all")
+   system("conda install " + _pkg + " ; conda update --all")
 
 def fix_environment():
     from os import environ

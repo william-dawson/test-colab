edb583fcea8e5459e4456ebc70161a8d7483e22a
William Dawson <william.dawson@riken.jp>
2022-09-15 20:56:01 +0900

    One liner

--- nbup.py
+++ nbup.py
@@ -1,11 +1,16 @@
-from os import system
 _base = "https://github.com/william-dawson/test-colab/raw/main/packages/"
 _pkg = "bigdft-suite-1.9.3_rc.2-mpi_openmpi_py37h008c211_0.tar.bz2"
 
+def skip():
+   from os import environ
+   return "BIGDFT_ROOT" in environ
+
 def download():
+    from os import system
     system("wget " + _base + _pkg)
 
 def install_colab():
+    from os import system
     system("pip install -q condacolab")
 
 def setup_colab():
@@ -13,9 +18,18 @@ def setup_colab():
     condacolab.install()
 
 def install_bigdft():
-   system("conda install " + _pkg + " ; conda update --all")
+    from os import system
+    system("conda install " + _pkg + " ; conda update --all")
 
 def fix_environment():
     from os import environ
     environ["BIGDFT_ROOT"] = "/usr/local/bin"
+
+# Run them all
+if not skip():
+    download()
+    install_colab()
+    setup_colab()
+    install_bigdft()
+    fix_environment()
 
